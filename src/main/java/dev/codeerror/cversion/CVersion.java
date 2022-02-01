1249689757d0e2a0da019075803db8de657a0608
CodeTheDev <code@codeslab.ca>
2022-01-31 23:44:20 -0500

    Code cleanup
    - Cleaned up code in all classes. (Removed excessive whitespace, code duplication, etc.)

--- src/main/java/dev/codeerror/cversion/CVersion.java
+++ src/main/java/dev/codeerror/cversion/CVersion.java
@@ -1,9 +1,12 @@
 package dev.codeerror.cversion;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerOptions;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.WrappedServerPing;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -13,21 +16,18 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.ListenerOptions;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.WrappedServerPing;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class CVersion extends JavaPlugin implements CommandExecutor, TabCompleter {
 
 	private static final String chatPrefix = ChatColor.DARK_GRAY + "[" + ChatColor.BLUE + "CVersion" + ChatColor.DARK_GRAY + "] ";
 
-	private List<String> subcommands = Arrays.asList("help", "reload", "set");
-	private List<String> setSubcommands = Arrays.asList("version", "protocol");
-	private List<String> blank = new ArrayList<>();
+	private final List<String> subcommands = Arrays.asList("help", "reload", "set");
+	private final List<String> setSubcommands = Arrays.asList("version", "protocol");
+	private final List<String> blank = new ArrayList<>();
 
 	private String version = this.getConfig().getString("version-string", "Epic Server");
 	private int protocol = this.getConfig().getInt("protocol-number", 47);
@@ -37,35 +37,28 @@ public class CVersion extends JavaPlugin implements CommandExecutor, TabComplete
 		
 		this.saveDefaultConfig();
 
-		ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, Arrays.asList(PacketType.Status.Server.SERVER_INFO), ListenerOptions.ASYNC) {
-
+		ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, Collections.singletonList(PacketType.Status.Server.SERVER_INFO), ListenerOptions.ASYNC) {
 			@Override
 			public void onPacketSending(PacketEvent event) {
-
 				pingPacket(event.getPacket().getServerPings().read(0));
-
 			}
-
 		});
 
 		this.getCommand("cversion").setExecutor(this);
 		this.getCommand("cversion").setTabCompleter(this);
+
 		this.getLogger().info("CVersion v1.0 has been successfully enabled! Created by CodeError.");
 
 	}
 
 	@Override
 	public void onDisable() {
-
 		this.getLogger().info("CVersion v1.0 has been disabled.");
-
 	}
 
 	private void pingPacket(WrappedServerPing ping) {
-
 		ping.setVersionName(version);
 		ping.setVersionProtocol(protocol);
-
 	}
 
 	@Override
@@ -73,37 +66,25 @@ public class CVersion extends JavaPlugin implements CommandExecutor, TabComplete
 
 		if (args.length == 0) {
 
-			if (!(sender instanceof Player)) {
-
-				return false;
-
-			}
+			if (!(sender instanceof Player)) return false;
 
 			sender.sendMessage(chatPrefix + ChatColor.GOLD + "CVersion v1.0 - Created by " + ChatColor.RED + "CodeError");
 			sender.sendMessage(chatPrefix + ChatColor.GRAY + "Plugin Help: " + ChatColor.WHITE + "/cversion help");
 
 			return true;
 
-		}
-
-		else {
+		} else {
 
 			if (args[0].equalsIgnoreCase("help")) {
 
-				if (!(sender instanceof Player)) {
-
-					return false;
-
-				}
+				if (!(sender instanceof Player)) return false;
 
 				sender.sendMessage(chatPrefix + ChatColor.GOLD + "CVersion v1.0 - " + ChatColor.RED + "Plugin Help");
 				sender.sendMessage(chatPrefix + ChatColor.GRAY + "/cversion reload - " + ChatColor.WHITE + "Reloads the plugin.");
 
 				return true;
 
-			}
-
-			else if (args[0].equalsIgnoreCase("reload")) {
+			} else if (args[0].equalsIgnoreCase("reload")) {
 
 				if (!(sender instanceof Entity)) {
 
@@ -113,85 +94,51 @@ public class CVersion extends JavaPlugin implements CommandExecutor, TabComplete
 
 					return true;
 
-				}
-
-				else if (sender instanceof Player && sender.hasPermission("cversion.reload")) {
+				} else if (sender instanceof Player && sender.hasPermission("cversion.reload")) {
 
 					this.reloadConfig();
 					this.getConfig();
 					this.getLogger().info("Plugin configuration has been successfully reloaded. All changes have been applied.");
 
 					sender.sendMessage(chatPrefix + ChatColor.GREEN + "Plugin configuration has been successfully reloaded. All changes have been applied.");
-
 					return true;
 
 				}
 
 				sender.sendMessage(chatPrefix + ChatColor.DARK_RED + "You do not have the appropriate permissions to perform this command.");
-
 				return true;
 
-			}
+			} else if (args[0].equalsIgnoreCase("set")) {
 
-			else if (args[0].equalsIgnoreCase("set")) {
-
-				if (!(sender instanceof Player)) {
-
-					return false;
-
-				}
+				if (!(sender instanceof Player)) return false;
 
 				if (args.length == 1) {
 
 					sender.sendMessage(chatPrefix + ChatColor.RED + "Too few args! Please specify which value you wish to set!");
-
 					return true;
 
-				}
+				} else if (args[1].equalsIgnoreCase("version")) {
 
-				else if (args[1].equalsIgnoreCase("version")) {
-
-					if (!(sender instanceof Player)) {
-
-						return false;
-
-					}
-
-					else if (sender instanceof Player && sender.hasPermission("cversion.set.version")) {
+					if (sender.hasPermission("cversion.set.version")) {
 
 						if (args.length == 2) {
 
 							sender.sendMessage(chatPrefix + ChatColor.RED + "Too few args! Please specify a non-empty value!");
-
 							return true;
 
-						}
-						
-						else if (args.length == 3 && args[2].equalsIgnoreCase("default")) {
-							
+						} else if (args.length == 3 && args[2].equalsIgnoreCase("default")) {
+
 							version = "Epic Server";
-							sender.sendMessage(chatPrefix + ChatColor.GREEN + "Version string successfully set to: " + ChatColor.GRAY + version);
 
+							sender.sendMessage(chatPrefix + ChatColor.GREEN + "Version string successfully set to: " + ChatColor.GRAY + version);
 							return true;
-							
+
 						}
 
 						StringBuilder versionBuilder = new StringBuilder();
 
 						for (int i = 2; i < args.length; i++) {
-
-							if (i < args.length) {
-
-								versionBuilder.append(args[i] + " ");
-
-							}
-
-							else if (i == args.length - 1) {
-
-								versionBuilder.append(args[i]);
-
-							}
-
+							versionBuilder.append(args[i]).append(" ");
 						}
 
 						version = ChatColor.translateAlternateColorCodes('&', new String(versionBuilder));
@@ -199,70 +146,50 @@ public class CVersion extends JavaPlugin implements CommandExecutor, TabComplete
 						this.getConfig().set("version-string", version);
 						this.reloadConfig();
 						this.getConfig();
-						sender.sendMessage(chatPrefix + ChatColor.GREEN + "Version string successfully set to: " + ChatColor.GRAY + version);
 
+						sender.sendMessage(chatPrefix + ChatColor.GREEN + "Version string successfully set to: " + ChatColor.GRAY + version);
 						return true;
 
 					}
 
 					sender.sendMessage(chatPrefix + ChatColor.DARK_RED + "You do not have the appropriate permissions to perform this command.");
-
 					return true;
 
-				}
+				} else if (args[1].equalsIgnoreCase("protocol")) {
 
-				else if (args[1].equalsIgnoreCase("protocol")) {
-
-					if (!(sender instanceof Player)) {
-
-						return false;
-
-					}
-
-					else if (sender instanceof Player && sender.hasPermission("cversion.set.protocol")) {
+					if (sender.hasPermission("cversion.set.protocol")) {
 
 						if (args.length == 2) {
 
 							sender.sendMessage(chatPrefix + ChatColor.RED + "Too few args! Please specify a non-empty integer value!");
-
 							return true;
 
-						}
-						
-						else if (args.length == 3 && args[2].equalsIgnoreCase("default")) {
+						} else if (args.length == 3 && args[2].equalsIgnoreCase("default")) {
 							
 							protocol = 47;
-							sender.sendMessage(chatPrefix + ChatColor.GREEN + "Version string successfully set to: " + ChatColor.GRAY + protocol);
 
+							sender.sendMessage(chatPrefix + ChatColor.GREEN + "Version string successfully set to: " + ChatColor.GRAY + protocol);
 							return true;
 							
 						}
 
 						try {
-
 							protocol = Integer.parseInt(args[2]);
-
-						}
-
-						catch (NumberFormatException e) {
-
+						} catch (NumberFormatException e) {
 							sender.sendMessage(chatPrefix + ChatColor.RED + "Invalid protocol number specified! Please check " + ChatColor.YELLOW + "https://wiki.vg/Protocol_version_numbers " + ChatColor.RED + "for valid protocol numbers.");
-
 							return true;
-
 						}
 
 						this.getConfig().set("protocol-number", protocol);
 						this.reloadConfig();
 						this.getConfig();
-						sender.sendMessage(chatPrefix + ChatColor.GREEN + "Protocol number successfully set to: " + ChatColor.GRAY + protocol);
 
+						sender.sendMessage(chatPrefix + ChatColor.GREEN + "Protocol number successfully set to: " + ChatColor.GRAY + protocol);
 						return true;
 
 					}
 
 					sender.sendMessage(chatPrefix + ChatColor.DARK_RED + "You do not have the appropriate permissions to perform this command.");
-
 					return true;
 
 				}
@@ -272,28 +199,18 @@ public class CVersion extends JavaPlugin implements CommandExecutor, TabComplete
 		}
 
 		sender.sendMessage(chatPrefix + ChatColor.RED + "Unknown subcommand. " + ChatColor.GRAY + "/cversion help");
-
 		return true;
 
 	}
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-
 		if (args.length == 1) {
-
 			return subcommands;
-
-		}
-
-		else if (args.length == 2 && args[0].equalsIgnoreCase("set")) {
-
+		} else if (args.length == 2 && args[0].equalsIgnoreCase("set")) {
 			return setSubcommands;
-
 		}
-
 		return blank;
-
 	}
 
 }
